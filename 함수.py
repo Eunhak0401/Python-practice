bf873fab3dff6d0afd2a2cd38755953f4826b7ec
Eunhak0401 <74fresh4490@gmail.com>
2024-05-19 01:52:49 +0900

    함수 공부

--- 함수.py
+++ 함수.py
@@ -1,4 +1,7 @@
-def cm():
+if __name__ == '__main__':
+#길이 함수
+ '''
+ def cm():
     print( num,'mm -->',num * 0.1 ,'cm')
 def m():
     print( num,'mm -->',num * 0.001 ,'m')
@@ -13,4 +16,32 @@ def calculator():
 
 
 num = int(input('숫자를 입력하시오:'))
+calculator()
+'''
+ #연산자 함수
+def add():
+    print('덧셈결과:', num1 + num2)
+def sub():
+    print('뺄셈결과:', num1 - num2)
+def mul():
+    print('곱셈결과:', num1 * num2)
+def div():
+    print('나눗셈결과:', num1 / num2)
+def remain():
+    print('나머지:', num1 % num2)
+def calculator():
+        if (Operator == 1):
+                add()
+        if (Operator == 2):
+                sub()
+        if (Operator == 3):
+                mul()
+        if (Operator == 4):
+                div()
+        if (Operator == 5):
+                remain()
+
+num1 = int(input("첫 번째 숫자를 입력하시오: "))
+num2 = int(input("두 번째 숫자를 입력하시오: "))
+Operator = int(input("연산자를 선택하세요: "))
 calculator()
